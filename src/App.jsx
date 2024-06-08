d6aa993740d3bd8602a7868a6dca724d60fd0035
Shani Soni <shanisoni7084@gmail.com>
2024-06-08 10:48:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AverageCalculator from "./AverageCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <AverageCalculator />
+    </div>
+  );
+}
+
+export default App;
